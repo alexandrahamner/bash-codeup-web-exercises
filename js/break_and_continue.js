48971fced911e9909d799fb3e70d7eeb0395028f
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-07-30 16:22:04 -0500

    added use strict and IIFE

---
+++ js/break_and_continue.js
@@ -0,0 +1,12 @@
+
+// 1.)
+
+while(true) {
+    var userNumber = prompt("Please submit an odd number between 1 and 50.");
+    if (userNumber % 2 !== 1) {
+        console.log("Invalid Input!");
+        break;
+    }
+}
+
+
