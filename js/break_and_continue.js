bf41adc82628ac3610bce10f9205fceef71b0069
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-07-30 17:39:57 -0500

    added solution to the exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,14 +1,22 @@
 "use strict";
 (function () {
+
+
 // 1.)
 
-    while (true) {
-        var userNumber = prompt("Please submit an odd number between 1 and 50.");
-        if (userNumber % 2 !== 1) {
-            console.log("Invalid Input!");
-            break;
+    do {
+        var userNumber = prompt("Please submit an odd number between 1 and 50.")
+    } while ((userNumber % 2 == 0) || (userNumber > 49) || (userNumber < 0));
+
+    for (var x = 1; x <= 49; x += 2) {
+        if (x == userNumber) {
+            console.log("Yikes! Skipping: " + userNumber);
+            continue;
         }
+        console.log("Here is an odd number: " + x);
     }
+
+
 
 
 }) ();
