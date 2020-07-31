3a2608d99ea0f403918baa78e3d82b6ad18b0ef0
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-07-31 10:55:37 -0500

    added instructor's solution for the exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -4,8 +4,10 @@
 
 // 1.)
 
+    var userNumber;
+
     do {
-        var userNumber = prompt("Please submit an odd number between 1 and 50.")
+        userNumber = parseInt(prompt("Please submit an odd number between 1 and 50."))
     } while ((userNumber % 2 == 0) || (userNumber > 49) || (userNumber < 0));
 
     for (var x = 1; x <= 49; x += 2) {
@@ -15,6 +17,36 @@
         }
         console.log("Here is an odd number: " + x);
     }
+
+
+    // // instructor's solution
+    // while (true) {
+    //     userNumber = parseInt(prompt("Please submit an odd number between 1 and 50."));
+    //     if ((userNumber % 2 !== 0) && (userNumber < 50) && (userNumber > 0)) {
+    //         break;
+    //     }
+    // }
+    //
+    // console.log("Number to Skip: " + userNumber);
+    // console.log();
+    //
+    // for (var i = 1; i < 50; i += 2) {
+    //     if (i === userNumber) {
+    //         console.log("Yikes! Skipping the number: " + userNumber);
+    //         continue;
+    //     }
+    //     console.log("Here is an odd number: " + i);
+    // }
+    //
+    // var i = 1
+    // while (i < 50)  {
+    //     if (i === userNumber) {
+    //         console.log("Yikes! Skipping the number: " + userNumber);
+    //         continue;
+    //     }
+    //     console.log("Here is an odd number: " + i);
+    //     i += 2;
+    // }
 
 
 
