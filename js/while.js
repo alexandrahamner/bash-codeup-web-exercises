1ecf2972c0cedb5657ea090b3f3e9e02fe46e3c2
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-07-30 23:27:14 -0500

    added solution to exercise 2

--- js/while.js
+++ js/while.js
@@ -2,10 +2,36 @@
 (function () {
 
 //  1.)
+
     var x = 2
     console.log(x);
     while (x < 65536) {
         console.log(x += x);
     }
+
+
+// 2.)
+
+    var amountToSell = Math.floor((Math.random() * 50) + 50);
+    console.log("This is how many ice cream cones I need to sell: " + amountToSell);
+
+        do {
+            var conesBought = Math.floor(Math.random() * 5) + 1;
+            var remainingCones = amountToSell -= conesBought
+            console.log(remainingCones);
+            if (remainingCones !== 0) {
+                if (conesBought > amountToSell) {
+                    console.log("I cannot sell you " + conesBought + " I only have " + remainingCones);
+                    break;
+                } else {
+                    console.log(conesBought + " cones sold.");
+                }
+            } else {
+                console.log("Yay I have no more left!");
+                break;
+            }
+        } while (amountToSell > 0);
+
+
 
 }) ();
