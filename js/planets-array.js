65fe37c517e1f1479392938abe856928dc1aaf0b
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-08-03 11:00:54 -0500

    completed the planets-array exercise and updated the planets-js HTML file.

--- js/planets-array.js
+++ js/planets-array.js
@@ -18,23 +18,31 @@
      * the step that it describes
      */
 
+    planets.unshift("The Sun");
     console.log('Adding "The Sun" to the beginning of the planets array.');
     console.log(planets);
 
+    planets.push("Pluto");
     console.log('Adding "Pluto" to the end of the planets array.');
     console.log(planets);
 
+    planets.shift();
     console.log('Removing "The Sun" from the beginning of the planets array.');
     console.log(planets);
 
+    planets.pop();
     console.log('Removing "Pluto" from the end of the planets array.');
     console.log(planets);
 
+    var earthIndex = planets.indexOf("Earth");
     console.log('Finding and logging the index of "Earth" in the planets array.');
+    console.log("The index of 'Earth' is: " + earthIndex);
 
+    planets.reverse();
     console.log("Reversing the order of the planets array.");
     console.log(planets);
 
+    planets.sort();
     console.log("Sorting the planets array.");
     console.log(planets);
 })();
