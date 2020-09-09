cba57ddb269b1894423ec8d43cdc8fb35fff10f2
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-09-09 09:59:40 -0500

    feat: completed the selectors jQuery exercise.

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -2,6 +2,15 @@
 
 $(document).ready(function() {
 
-    alert("DOM is finished loading!")
+    // var idContents = $('#container').html();
+    // alert(idContents);
 
+    // $('.codeup').css("border","red solid 1px");
+
+    $('li').css("font-size","20px");
+
+    $('h1 , li , p').css("color", "purple");
+
+    var h1Contents = $("h1").html();
+    alert(h1Contents);
 });
