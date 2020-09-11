f02ef9256890eaae2f260c6e52824958a2f547af
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-09-10 22:58:28 -0500

    feat: change the background when the user types in the cheat code. also changed the font case when a required key is pressed.

--- js/konami-exercise.js
+++ js/konami-exercise.js
@@ -19,6 +19,36 @@ $(document).ready(function () {
 
         //compares the key the user pressed to the konami key at index based on count.
         if (userKey == requiredKey) {
+            if (count == 0) {
+                $("#k").css("text-transform","uppercase");
+            }
+            if (count == 1) {
+                $("#o").css("text-transform","uppercase");
+            }
+            if (count == 2) {
+                $("#n").css("text-transform","uppercase");
+            }
+            if (count == 3) {
+                $("#a").css("text-transform","uppercase");
+            }
+            if (count == 4) {
+                $("#m").css("text-transform","uppercase");
+            }
+            if (count == 5) {
+                $("#i").css("text-transform","uppercase");
+            }
+            if (count == 6) {
+                $("#c").css("text-transform","uppercase");
+            }
+            if (count == 7) {
+                $("#o2").css("text-transform","uppercase");
+            }
+            if (count == 8) {
+                $("#d").css("text-transform","uppercase");
+            }
+            if (count == 9) {
+                $("#e").css("text-transform","uppercase");
+            }
             //if the keys match, the count goes up by one, and continues to check for matched keys.
             count++
             //once the count equals the length of konamiKeys array, the cheat code is activated.
@@ -29,14 +59,23 @@ $(document).ready(function () {
                 count = 0;
             }
         } else {
-            //if the userKey and konami key do not match, the count starts over.
+            //if the userKey and konami key do not match the text turns red and the count starts over.
+            $("h1").css('color','red');
+            setTimeout(function () {
+                $("h1").css('color', 'black');
+            }, 500)
             count = 0;
         }
 
     });
 
+    //TODO add some effect for when the user is typing
+
+
     //TODO make something cool happen!
     function cheatActivated() {
-        alert("Success!");
+        $("body").css({"background": "linear-gradient(-45deg, #eaac8b, #6d597a, #eaac8b, #e56b6f)", "background-size": "300% 300%", "animation": "gradient 20s ease infinite"});
+        $("#password-text").text("success");
     }
+
 })
