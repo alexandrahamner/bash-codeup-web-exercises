5127484fdb56a538002666411b0cb02f09ac09ec
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-09-16 09:48:06 -0500

    feat: working on geocding for map.

--- js/weather_custom_script.js
+++ js/weather_custom_script.js
@@ -1,14 +1,36 @@
 "use strict";
 (function () {
-    var weatherURL = "https://api.openweathermap.org/data/2.5/forecast";
+    mapboxgl.accessToken = mapboxToken;
 
-    $.get(weatherURL, {
-        "APPID": Open_Weather_APIID,
-        "q": "San Antonio, US",
-        "units": "imperial"
-    }).done(function (data) {
-        console.log(data);
-    })
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/dark-v10', // stylesheet location
+        center: [-98, 40], // starting position [lng, lat]
+        zoom: 3// starting zoom
+    });
+
+    var userInput =
+    function placeMarker(info, token, map) {
+        geocode(info.address, token).then(function(coordinates) {
+            var marker = new mapboxgl.Marker()
+                .setLngLat(coordinates)
+                .addTo(map)
+        });
+    }
+
+
+    var weatherURL = "https://api.openweathermap.org/data/2.5/forecast";
+    function getWeather(lng,lat) {
+        $.get(weatherURL, {
+            "APPID": Open_Weather_APIID,
+            "lon": lng,
+            "lat": lat,
+            "units": "imperial"
+        }).done(function (data) {
+            console.log(data);
+        })
+    }
+
 
 
 }());
