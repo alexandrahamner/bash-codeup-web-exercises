f9a0d2ec377fcfb4c31d7d92abddd9e28978583f
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-09-21 13:40:22 -0500

    feat: completed the exercise, when a username is passed into the function, the date of the last commit is console logged.

---
+++ js/promises.js
@@ -0,0 +1,38 @@
+
+
+
+//     fetch(url, {headers: {'Authorization': 'token b5dc99563baece3d245c9f3d77c758155e8fbe5f'}})
+//         .then(response => response.json())
+//         .then(users => {
+//             users.forEach(userObj => {
+//                 usernames.push(userObj.login);
+//             })
+//         })
+//         .catch(error => console.error(error));
+//
+// console.log(usernames);
+
+function getLastCommit (username) {
+    let url = ("https://api.github.com/users/" + username + "/events/public");
+    fetch(url, {headers: {'Authorization': 'token b5dc99563baece3d245c9f3d77c758155e8fbe5f'}})
+        .then(response => response.json()
+            .then(events => {
+                for(let i = 0; i < events.length; i++) {
+                    if(events[i].type === "PushEvent") {
+                        console.log("The last commit was made: ", events[i].created_at);
+                        break;
+                    }
+                }
+            })
+        )
+}
+
+getLastCommit("alexandrahamner");
+
+
+
+
+
+///users/:username/events/public
+
+//created_at: "2020-09-18T18:52:45Z"
