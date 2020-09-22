404fed71fb2c7059eaa48640ad122377bd46bf7a
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-09-21 21:20:34 -0500

    feat: added a form so the user can input their own username to check when they last made a commit.

--- js/promises.js
+++ js/promises.js
@@ -1,19 +1,26 @@
 function getLastCommit (username) {
     let url = ("https://api.github.com/users/" + username + "/events/public");
-    fetch(url, {headers: {'Authorization': githubToken}})
-        .then(response => response.json()
+    return fetch(url, {headers: {'Authorization': githubToken}})
+        .then((response) => response.json())
             .then(events => {
                 for(let i = 0; i < events.length; i++) {
                     if(events[i].type === "PushEvent") {
-                        console.log("The last commit was made: ", events[i].created_at);
-                        break;
+                       return events[i].created_at
                     }
                 }
-            })
-        )
+            });
 }
 
 getLastCommit("alexandrahamner");
+
+document.querySelector("#getUsername").addEventListener("click", function (e) {
+    e.preventDefault();
+    const userInput = document.querySelector("#username").value;
+    getLastCommit(userInput).then((date) => {
+        const div = document.querySelector("#commit-container");
+        div.innerHTML = "Your last commit was on: " + date.substring(5,10) + "-" + date.substring(0,4);
+    })
+})
 
 ///users/:username/events/public
 
